6594b84c8765586efeb573712c4c1852a085b2fb
badwordking <51959848+badwordking@users.noreply.github.com>
2019-11-28 23:28:23 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+#set password
+$password_of_script = "lucis";
+
+#set your config
+#akun1
+$refresh_token[] = "xxxxx";
+$device_id[] = "xxxx";
+
+#akun2
+$refresh_token[] = "xxxxxx";
+$device_id[] = "xxxxx";
+
+
+#akun3 dan setersusnya..👇
